36140324b8df05aec4215906045660bfd38cf054
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2023-10-02 23:48:32 +0300

    Add files via upload

---
+++ Link loop C#/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace URLOpener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the URL:"); 
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Please Input the Minutes (Minute): ");
+            int duration = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 999; i++)
+            {
+                
+                int sleep = duration * 60 * 1000;
+
+                Process.Start(url);
+
+                Thread.Sleep(sleep); // Sleep for 10 minutes (10 * 60 * 1000 milliseconds)
+            }
+                
+           
+        }
+    }
+}
